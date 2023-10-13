5e41a349e0577b9634df1a839188b3eed7d24fbd
Replit user <>
2023-10-13 16:03:18 +0000

    Project edited

---
+++ Level07/The_Four_Sisters_and_the_Duckbear/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace The_Four_Sisters_and_the_Duckbear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of eggs: ");
+            int numberOfEggs = Convert.ToInt32(Console.ReadLine());
+            int numberOfDuckbearsEggs = numberOfEggs % 4;
+            int numberOfSistersEggs = (numberOfEggs - numberOfDuckbearsEggs) / 4;
+
+            Console.WriteLine("Number of Sisters Eggs: " + numberOfSistersEggs);
+            Console.WriteLine("Number of Duckbears Eggs: " + numberOfDuckbearsEggs);
+        }
+    }
+}
---
+++ Level09/Repairing_the_Clocktower/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Repairing_the_Clocktower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool isEven = number % 2 == 0 ? true : false;
+
+            string result = isEven ? "Tick" : "Tock";
+
+            Console.WriteLine(result);
+        }
+    }
+}
