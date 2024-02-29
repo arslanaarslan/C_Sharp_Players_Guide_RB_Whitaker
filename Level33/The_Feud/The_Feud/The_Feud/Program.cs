b12ccb263486889df389ea70999be61ab9176927
Arslan Arslan <arslanaarslan@gmail.com>
2024-02-29 23:04:54 +0300

    Add 'The_Feud' project and rearrange 'The_Time_In_The_Cavern' files
    Added various files necessary for the 'The_Feud' solution, including xml configurations, .gitignore, new class files, and a '.csproj' file to establish the new project. Additionally, moved 'The_Time_In_The_Cavern' files from Level32 to Level30. This commit primarily covers project setup and file organization for better project and level management.

---
+++ Level33/The_Feud/The_Feud/The_Feud/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IField;
+using McDroid;
+
+Sheep sheep = new Sheep();
+IField.Pig pig1 = new IField.Pig();
+
+Cow cow = new Cow();
+McDroid.Pig pig2 = new McDroid.Pig();
+
