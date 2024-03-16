ecbdb10807239c4f527cbd320de01f9af9e15f19
Arslan Arslan <arslanaarslan@gmail.com>
2024-03-16 16:49:59 +0300

    Add Level34 Safer_Number_Crunching project
    Created a new project in Level34 named Safer_Number_Crunching, with a .Net 8.0 framework and a user-friendly console interface to accept integer inputs. Removed unnecessary folder inclusion in the SolutionForCSharpPlayersGuide and updated the indexLayout.xml to include Level34.

---
+++ Level34/Safer_Number_Crunching/Safer_Number_Crunching/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    Console.Write("Enter the value: ");
+    string inputValue = Console.ReadLine();
+    if (int.TryParse(inputValue, out int value))
+    {
+        Console.WriteLine($"Entered Number is: {value}");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Please enter integer number!!!");
+    }
+}
+
+Console.WriteLine("Program ended.");
